e4c812f967061f8064b3dbaa5b7b3c6e87ac51a1
itsadeadh2 <itsadeadh2@gmail.com>
2020-01-19 15:51:41 -0400

    feat: add ci/cd

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+docker build -t itsadeadh2/complex-client:latest -t itsadeadh2/complex-client:$SHA -f ./client/Dockerfile ./client
+docker build -t itsadeadh2/complex-server:latest -t itsadeadh2/comple-server:$SHA -f ./server/Dockerfile ./server
+docker build -t itsadeadh2/complex-worker:latest -t itsadeadh2/complex-worker:$SHA -f ./worker/Dockerfile ./worker
+docker push itsadeadh2/complex-client
+docker push itsadeadh2/complex-server
+docker push itsadeadh2/complex-worker
+kubectl apply -f  k8s
+kubectl set image deployments/server-deployment server=itsadeadh2/complex-server:$SHA
+kubectl set image deployments/client-deployment server=itsadeadh2/complex-client:$SHA
+kubectl set image deployments/worker-deployment server=itsadeadh2/complex-worker:$SHA
