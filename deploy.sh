72845052fdf593a3eb394a65d3f7c8f81e97628a
itsadeadh2 <itsadeadh2@gmail.com>
2020-01-19 16:11:01 -0400

    fix: typo

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 docker build -t itsadeadh2/complex-client:latest -t itsadeadh2/complex-client:$SHA -f ./client/Dockerfile ./client
-docker build -t itsadeadh2/complex-server:latest -t itsadeadh2/comple-server:$SHA -f ./server/Dockerfile ./server
+docker build -t itsadeadh2/complex-server:latest -t itsadeadh2/complex-server:$SHA -f ./server/Dockerfile ./server
 docker build -t itsadeadh2/complex-worker:latest -t itsadeadh2/complex-worker:$SHA -f ./worker/Dockerfile ./worker
 docker push itsadeadh2/complex-client
 docker push itsadeadh2/complex-server
